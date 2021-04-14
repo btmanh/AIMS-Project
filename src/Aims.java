064c278ab90b71d07ca8efe15196d21c6e2bfe88
Bui Thi Mai Anh <maianht2@gmail.com>
2021-04-14 16:57:33 +0700

    Example Commit

--- src/Aims.java
+++ src/Aims.java
@@ -1,9 +1,11 @@
+import javax.xml.transform.Source;
 
 public class Aims {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("AIMS PROJECT");
+		System.out.println("Commit");
 	}
 
 }
