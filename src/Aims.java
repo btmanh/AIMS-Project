fd7513018c09a0f1516798e25b33cfb78b9bb1b5
Bui Thi Mai Anh <maianht2@gmail.com>
2021-05-05 17:03:04 +0700

    Update AIMS class

--- src/Aims.java
+++ src/Aims.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.xml.transform.Source;
 
 public class Aims {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("AIMS PROJECT");
-		System.out.println("Commit");
+		
+		//Write command line menu 
+		//Choose 1: create new order
+		//Choose 2: add item to order
+		//Choose 3: display order info
 	}
 
 }
